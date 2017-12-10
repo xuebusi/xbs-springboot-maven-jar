cab445ec99c0e3f14502e2a7054bad2e22a94805
xuebusi <xbs1019@126.com>
2017-12-10 20:02:26 +0800

    Update PersonController.java

--- src/main/java/com/xuebusi/springboot/maven/controller/PersonController.java
+++ src/main/java/com/xuebusi/springboot/maven/controller/PersonController.java
@@ -45,6 +45,7 @@ public class PersonController {
 		Person person = personService.selectByPrimaryKey(personId);
 		ResultModel<Object> resultModel = new ResultModel<>();
 		resultModel.setCode(200);
+		resultModel.setMessage("github");
 		resultModel.setData(person);
 		return resultModel;
 	}
